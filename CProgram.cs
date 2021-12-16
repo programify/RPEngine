842bfd0b801ee37d508943f2347a9f70df57d818
Clive Hudson <github@programify.com>
2021-12-16 14:43:55 +0000

    Works Prerelease 0.1

--- CProgram.cs
+++ CProgram.cs
@@ -45,6 +45,7 @@
 using System;
 using System.Configuration;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -65,12 +66,16 @@ namespace NSEngineApp
 //-----------------------------------------------------------------------------
 class CProgram
 {
+     public static byte[] gaPage400 ;
 
      public static bool gbfRunServer ;
 
+     public static string gstrDomains ;
+     public static string gstrError400 ;
      public static string gstrProxyIp ;
      public static string gstrServerIp ;
 
+     public static string[] gastrDomains ;
      public static string gstrHttp ;
      public static string gstrHttps ;
      public static string gstrServer ;
@@ -119,12 +124,19 @@ public static bool GetConfiguration (string [] args)
 // Get network settings
      gstrProxyIp  = ConfigurationManager.AppSettings ["ReverseProxyIP"] ;
      gstrServerIp = ConfigurationManager.AppSettings ["LocalizedServerIP"] ;
+     gstrDomains  = ConfigurationManager.AppSettings ["DomainNames"] ;
 
 // Construct reverse proxy address (this code runs at this location)
      gstrHttp  = "http://"  + gstrProxyIp + ":80/" ;
      gstrHttps = "https://" + gstrProxyIp + ":443/" ;
 // Construct base address of localized server
      gstrServer = "http://"  + gstrServerIp + ":80/" ;
+
+// Convert DomainNames into an array of strings
+     gastrDomains = gstrDomains.Split (',') ;
+     for (var iIndex = 0 ; iIndex < gastrDomains.Length ; iIndex ++ )
+          gastrDomains [iIndex] = gastrDomains [iIndex].Trim () ;
+          
      return true ;
 }
 
@@ -149,8 +161,9 @@ public static bool StartReverseProxy ()
      }
 
      Console.ForegroundColor = ConsoleColor.Cyan ;
-     Console.WriteLine ("Hosting Reverse Proxy on {0}", gstrProxyIp) ;
-     Console.WriteLine ("Fronting True Server at  {0}", gstrServerIp) ;
+     Console.WriteLine ("Monitoring the Domain Name {0}", gstrDomains) ;
+     Console.WriteLine ("Hosting Reverse Proxy on   {0}", gstrProxyIp) ;
+     Console.WriteLine ("Fronting True Server at    {0}", gstrServerIp) ;
      Console.WriteLine ("") ;
      Console.ForegroundColor = ConsoleColor.White ;
      return true ;
@@ -166,6 +179,13 @@ public static bool ConnectLocalizedServer ()
      g_server.BaseAddress = new Uri (gstrServer, UriKind.Absolute) ;
      g_server.MaxResponseContentBufferSize = 1000000 ;
      g_server.Timeout = TimeSpan.FromSeconds (3) ;
+// Preload common error pages
+     Console.ForegroundColor = ConsoleColor.Cyan ;
+     gstrError400 = GetWebPage ("/errors/400.html") ;
+     Console.WriteLine ("") ;
+     Console.ForegroundColor = ConsoleColor.White ;
+// Convert page strings into byte arrays
+     gaPage400 = Encoding.ASCII.GetBytes (gstrError400) ;
      return true ;
 }
 
@@ -175,7 +195,9 @@ public static bool ConnectLocalizedServer ()
 //-----------------------------------------------------------------------------
 public static async Task HandleIncomingConnections ()
 {
-     int iStatus ;
+     int       iStatus ;
+     string    strQuery ;
+     string    strStatus ;
 
      HttpListenerContext      f_context ;
      HttpResponseMessage      f_message ;
@@ -192,34 +214,52 @@ public static async Task HandleIncomingConnections ()
           // Isolate request and response objects
                f_request  = f_context.Request ;
                f_response = f_context.Response ;
-
-          // Display debug info about the request
-               if (f_request.Url.LocalPath.Equals ("/"))
-                    Console.ForegroundColor = ConsoleColor.Yellow ;
-               if (f_request.Url.LocalPath.Contains (".html"))
-                    Console.ForegroundColor = ConsoleColor.Yellow ;
-               if (f_request.Url.LocalPath.Contains (".js"))
-                    Console.ForegroundColor = ConsoleColor.DarkYellow ;
-
-          // Report remote user accessing this proxy server
-               Console.Write (f_request.RemoteEndPoint.Address.ToString () + " ") ;
-               Console.Write (f_request.RemoteEndPoint.Port.ToString () + " ") ;
-               Console.Write (f_request.HttpMethod       + " ") ;
-               Console.Write (f_request.Url.ToString ()  + " ") ;
+               f_message  = null ;
+          // Default to bad request
+               iStatus    = (int) HttpStatusCode.InternalServerError ;
+               strStatus  = "Internal Server Error" ;
+          // Send info to console window without terminating the console line
+               DisplayEvent (f_request) ;
+          // Check if any known Domain Name is provided in client request
+               if (CheckDomains (f_request))
+                    strQuery = f_request.Url.PathAndQuery ;
+               else
+               {
+                    ErrorReflex (f_response, HttpStatusCode.BadRequest) ;
+                    goto close_channel ;
+               }
+          // Attempt to create connection to localized server to process redirected requests
+               try
+               {
+                    f_message = await g_server.GetAsync (strQuery) ;
+               // Report the HTTP Statuc Code
+                    iStatus   = ((int) f_message.StatusCode) ;
+               }
+               catch (Exception e)
+               {
+                    ReportException (e) ;
+               }
+          // Transfer http status code
+               f_response.StatusCode = iStatus ;
+               if (f_message != null)
+                    strStatus = f_message.StatusCode.ToString () ;
+          // Colourize server status code
+               if (iStatus <= 299)
+                    Console.ForegroundColor = ConsoleColor.Green ;
+               else
+                    Console.ForegroundColor = ConsoleColor.Red ;
+          // Report the HTTP status code
+               string strReport = string.Format ("[{0} - {1}]", iStatus, strStatus) ;
+               Console.WriteLine (strReport) ;
                Console.ForegroundColor = ConsoleColor.White ;
-
-          /// Add code to catch timeout exceptions (true server offline)
-          // Create connection to localized server to process redirected requests
-               f_message = await g_server.GetAsync (f_request.Url.PathAndQuery) ;
-
-               iStatus = ((int) f_message.StatusCode) ;
-
-               Console.WriteLine ("[" + iStatus.ToString() + " - " + f_message.StatusCode + "]") ;
+          // Abort request if page fetch failed
+               if (f_message == null)
+                    goto close_channel ;
 
           // Fetch the localized server's response (file content)
                byte[] abResponse = await f_message.Content.ReadAsByteArrayAsync () ;
-          // Transfer http status code
-               f_response.StatusCode = (int) f_message.StatusCode ;
+
+
           // Check if the asset was found on the localized server
                if (iStatus == (int) HttpStatusCode.OK)
                {
@@ -228,25 +268,193 @@ public static async Task HandleIncomingConnections ()
                          f_response.ContentType = null ;
                     else
                          f_response.ContentType = f_message.Content.Headers.ContentType.ToString () ;
-                    f_response.ContentEncoding = Encoding.UTF8 ;
+               // Apply default encoding if required
+                    if (f_response.ContentEncoding == null)
+                         f_response.ContentEncoding = Encoding.UTF8 ;
+
                     f_response.ContentLength64 = abResponse.LongLength ;
-                    f_response.KeepAlive       = true ;
+                    f_response.KeepAlive       = false ;
                }
           // Return data over the Internet to the client
                try
                {
+               // Browser may have dumped the connection which is why we try/catch this write
                     f_response.OutputStream.Write (abResponse, 0, abResponse.Length) ;
                }
                catch (Exception e)
                {
-               // Report failure to write back contents
-                    Console.ForegroundColor = ConsoleColor.Red ;
-                    Console.WriteLine ("***" + e.Message) ;
-                    Console.ForegroundColor = ConsoleColor.White ;
+                    ReportException (e) ;
+                    goto close_channel ;
                }
-          // Close channel to client
+
+     close_channel:
+
+          // Send the response and close channel to client
                f_response.Close () ;
      }
+}
+
+
+//-----------------------------------------------------------------------------
+//                                                                 CheckDomains
+//-----------------------------------------------------------------------------
+/*
+ *   Checks the "DomainNames" list for matching domain or sub-domain names.
+ *   Most hacks appear to use the IP address (since they are easier to generate.
+value="programify.com, www.programify.com, 10.0.0.5" />
+ */
+public static bool CheckDomains (HttpListenerRequest f_request)
+{
+// Reject if no hostname supplied
+     if (f_request.UserHostName == null)
+          return false ;
+// Accept is client-supplied domain name is recognised on white list
+     for (var iIndex = 0 ; iIndex < gastrDomains.Length ; iIndex ++ )
+     {
+          if (f_request.UserHostName.Equals (gastrDomains [iIndex]))
+               return true ;
+     }
+// Reject all other domain requests
+     return false ;
+}
+
+
+//-----------------------------------------------------------------------------
+//                                                                  ErrorReflex
+//-----------------------------------------------------------------------------
+/*
+ *   ErrorReflex() responds directly to the client with an error page that is
+ *   preloaded. This reduces the load on the system as the localized server
+ *   is not involved.
+ */
+public static void ErrorReflex (HttpListenerResponse response, HttpStatusCode status)
+{
+     byte[]    abBuffer ;
+     int       iStatus ;
+     string    strPage ;
+     string    strStatus ;
+
+// Init
+     iStatus = (int) status ;
+     response.StatusCode = iStatus ;
+// Distribute on HTTP status code
+     switch (iStatus)
+     {
+          case 400 :  abBuffer = gaPage400 ; strStatus = "Bad Request" ; break ;
+
+          default :
+               strStatus = "Internal Server Error" ;
+               strPage   = string.Format ("<h1>Internal Server Error</h1><p>HTTP Status Code <b>{0}</b> is not being handled.</p>", iStatus) ;
+               abBuffer  = Encoding.ASCII.GetBytes (strPage) ;
+               break ;
+     }
+// Report error on console
+     Console.ForegroundColor = ConsoleColor.Red ;
+     string strReport = string.Format ("<{0} - {1}>", iStatus, strStatus) ;
+     Console.WriteLine (strReport) ;
+     Console.ForegroundColor = ConsoleColor.White ;
+// Return data over the Internet to the client
+     try
+     {
+     // Browser may have dumped the connection which is why we try/catch this write
+          response.OutputStream.Write (abBuffer, 0, abBuffer.Length) ;
+     }
+     catch (Exception e)
+     {
+          ReportException (e) ;
+     }
+}
+
+
+//-----------------------------------------------------------------------------
+//                                                                 DisplayEvent
+//-----------------------------------------------------------------------------
+public static void DisplayEvent (HttpListenerRequest f_request)
+{
+     DateTime  dtNow = DateTime.Now ;
+
+// Get system timestamp
+     string strDate = string.Format ("{0:0000}-{1:00}-{2:00} ", dtNow.Year, dtNow.Month, dtNow.Day) ;
+     string strTime = string.Format ("{0:00}:{1:00}:{2:00}:{3:000}  ", dtNow.Hour, dtNow.Minute, dtNow.Second, dtNow.Millisecond) ;
+// Display datestamp in sortable format
+     Console.BackgroundColor = ConsoleColor.DarkBlue ; // rgb 0,0,32
+     Console.ForegroundColor = ConsoleColor.Gray ;
+     Console.Write (strDate) ;
+     Console.Write (strTime) ;
+     Console.ForegroundColor = ConsoleColor.White ;
+
+// Report IP address
+     Console.Write (f_request.RemoteEndPoint.Address.ToString ().PadRight (15) + " ") ;
+   //Console.Write (f_request.RemoteEndPoint.Port.ToString () + " ") ;
+
+// Colourize info about the request
+     if (f_request.Url.LocalPath.Equals ("/"))
+          Console.ForegroundColor = ConsoleColor.Yellow ;
+     if (f_request.Url.LocalPath.Contains (".html"))
+          Console.ForegroundColor = ConsoleColor.Yellow ;
+     if (f_request.Url.LocalPath.Contains (".js"))
+          Console.ForegroundColor = ConsoleColor.DarkYellow ;
+
+// Report remote user accessing this proxy server
+     Console.Write (string.Format ("{0} ", f_request.HttpMethod)) ;
+     Console.Write (string.Format ("{0} ", f_request.Url.ToString ())) ;
+// Report possible browser or bot identifier
+     if (f_request.UserAgent != null)
+     {
+          Console.ForegroundColor = ConsoleColor.Gray ;
+          Console.Write (string.Format ("({0})", f_request.UserAgent)) ;
+     }
+     Console.ForegroundColor = ConsoleColor.White ;
+}
+
+
+//-----------------------------------------------------------------------------
+//                                                                   GetWebPage
+//-----------------------------------------------------------------------------
+public static string GetWebPage (string strUri)
+{
+     string    strPage ;
+     string    strQuery ;
+     string    strStatus ;
+
+     Stream         response ;
+     StreamReader   reader ;
+     WebClient      wcClient ;
+
+// Init
+     strPage   = string.Format ("<p>Failed to preload <b>{0}</b> from localized server.</p>", strUri) ;
+     strStatus = "Failed" ;
+// Construct server query
+     strQuery = string.Format ("http://{0}{1}", gstrServerIp, strUri) ;
+// Attempt to fetch page from localized server
+     try
+     {
+          wcClient = new WebClient () ;
+          response = wcClient.OpenRead (strQuery) ;
+          reader   = new StreamReader (response, Encoding.ASCII) ;
+          strPage  = reader.ReadToEnd () ;
+          response.Close () ;
+          strStatus = "OK" ;
+     }
+     catch (Exception e)
+     {
+          ReportException (e) ;
+     }
+// Report action and outcome
+     Console.WriteLine ("Preload \"{0}\" ({1} Bytes) - [{2}]", strQuery, strPage.Length, strStatus) ;
+     return strPage ;
+}
+
+
+//-----------------------------------------------------------------------------
+//                                                              ReportException
+//-----------------------------------------------------------------------------
+public static void ReportException (Exception e)
+{
+// Report failure to write back contents
+     Console.ForegroundColor = ConsoleColor.Red ;
+     Console.WriteLine ("*** " + e.Message) ;
+     Console.ForegroundColor = ConsoleColor.White ;
 }
 
 
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Programify Ltd")]
 [assembly: AssemblyProduct("RPEngine")]
 [assembly: AssemblyCopyright("Copyright © 2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Programify")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
