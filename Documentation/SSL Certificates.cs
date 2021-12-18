25999eacaf6dd7744cb05ca2600f4f36b2ca8b6e
Clive Hudson <github@programify.com>
2021-12-18 14:31:03 +0000

    Add clarification notes.

--- Documentation/SSL Certificates.cs
+++ Documentation/SSL Certificates.cs
@@ -4,15 +4,31 @@
 //
 //*****************************************************************************
 /*
+ *   Reverse Proxy Engine.
+ *
+ *   (c) Copyright 2021, Programify Ltd.
+ *   All rights reserved.
+ *
+ *   This information is classified for Public Release and is provided without
+ *   any warranty, etc.
+ *
+ *   The following steps were used to generate self-certified SSL certificates
+ *   suitable for use on an Intranet or testing servers without the expense
+ *   and paperwork-load of buying SSL certificates. These notes were compiled
+ *   to help those operating in a Windows O/S environment.
+ *
+ *
  *   CONTENTS
  *
- *   1.   Make and deploy a self-certified SSL certificate
- *   2.   Generate PFX file from KEY and CRT.
+ *   [1]  Make and deploy a self-certified SSL certificate
+ *   [2]  Generate PFX file from KEY and CRT.
  */
 
 
 //*****************************************************************************
-//                             Make and deploy a self-certified SSL certificate
+//
+//   [1]                       Make and deploy a self-certified SSL certificate
+//
 //*****************************************************************************
 /*
  *   1.   Locate the latest Windows Developer's Kit, e.g.:
@@ -25,7 +41,7 @@
  *
  *        C:
  *        CD C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\
- *        makecert  -n "CN=ProgramifyCA"  -r  -sv D:\Apache24\ProgramifyCA.pvk  D:\Apache24\ProgramifyCA.cer
+ *        MAKECERT  -n "CN=ProgramifyCA"  -r  -sv D:\Apache24\ProgramifyCA.pvk  D:\Apache24\ProgramifyCA.cer
  *
  *             (Prompted to Create Private Key Password)
  *             Enter complex password, e.g.: 058d3d24-d1ac-4486-95ab-f32e27a4d6e9-ALPHA
@@ -34,7 +50,7 @@
  *
  *   4.   Use the CA certificate to generate an SSL Certificate:
  *
- *        makecert -sk D:\Apache24\ProgramifySSL -iv D:\Apache24\ProgramifyCA.pvk -n "CN=ProgramifySSL" -ic D:\Apache24\ProgramifyCA.cer D:\Apache24\ProgramifySSL.cer -sr localmachine -ss MY
+ *        MAKECERT -sk D:\Apache24\ProgramifySSL -iv D:\Apache24\ProgramifyCA.pvk -n "CN=ProgramifySSL" -ic D:\Apache24\ProgramifyCA.cer D:\Apache24\ProgramifySSL.cer -sr localmachine -ss MY
  *
  *             (Prompted to Enter Private Key Password)
  *             Enter same complex password, e.g.: 058d3d24-d1ac-4486-95ab-f32e27a4d6e9-ALPHA
@@ -91,7 +107,7 @@
  *
  *             Construct the NETSH command line using the SSL certificate's thumbprint and application's GUID:
  *
- *             "netsh  http add sslcert  ipport=10.0.0.5:443  certhash=57cce8979c356d4f8767ab5aa31a6cd22d02d119  appid={058d3d24-d1ac-4486-95ab-f32e27a4d6e9}"
+ *                  "NETSH  http add sslcert  ipport=10.0.0.5:443  certhash=57cce8979c356d4f8767ab5aa31a6cd22d02d119  appid={058d3d24-d1ac-4486-95ab-f32e27a4d6e9}"
  *
  *             Hopefully you will see the message : "SSL Certificate successfully added".
  *             If the add fails, look at how to "Generate PFX file from KEY and CRT" below.
@@ -101,7 +117,7 @@
  *
  *             Run this command to list all known SSL certificates, and the latest addition might be the last one listed.
  *
- *                  "netsh  http show sslcert"
+ *                  "NETSH  http show sslcert"
  *
  *             There appears to be 100 bindings of the same certificate to the range 0.0.0.0:44300 to 0.0.0.0:44399 on each system inspected so far.
  *
@@ -113,7 +129,9 @@
 
 
 //*****************************************************************************
-//                                           Generate PFX file from KEY and CRT
+//
+//   [2]                                     Generate PFX file from KEY and CRT
+//
 //*****************************************************************************
 /*
  *   Scenario:
@@ -142,7 +160,7 @@
  *
  *        Example for "programify.com":
  *
- *             "openssl  pkcs12 -export -out programify_com.pfx -inkey programify_com.key -in programify_com.crt"
+ *             "OPENSSL  pkcs12 -export -out programify_com.pfx -inkey programify_com.key -in programify_com.crt"
  *
  *        After pressing ENTER you will be asked to enter an Export Password, 
  *        and then to re-enter it to verify it. You can make up any password
